b3b28bf94e7707d0f9c4d3e367368d4718bcb9bc
Evan R <eunoia.github@gmail.com>
2014-10-18 21:16:19 -0700

    Bookmarklet points to production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
       @title = "#{value} | Vacation Caster"      
     end
   end
+
+  def bookmarklet_url
+  	if ENV['RAILS_ENV']=="production"
+  		"dry-cove-4518.herokuapp.com"
+  	else
+  		"localhost:3000"
+  	end
+  end
 end
